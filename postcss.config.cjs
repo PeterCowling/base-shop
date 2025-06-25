f77fc56affa7103a1e783346596e4d6154974a33
PeterCowling <peter.cowling1976@gmail.com>
2025-06-25 12:49:14 +0200

    fix lint

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,11 +1,11 @@
-/* /postcss.config.js  — single source of truth for every pkg/app */
-export default {
+/* /postcss.config.cjs  — single source of truth for every pkg/app */
+module.exports = {
   plugins: {
     // ⚠️ v4: the PostCSS plugin moved here
     "@tailwindcss/postcss": {},
 
     /* If you still rely on other PostCSS plugins keep them,
-       but remove autoprefixer & postcss‑import — v4 bundles both. */
+       but remove autoprefixer & postcss-import — v4 bundles both. */
     // 'postcss-nesting': {},
   },
 };

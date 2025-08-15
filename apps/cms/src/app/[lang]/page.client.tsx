715e2fbdd5e5428c19374a9a17c087e11862cd11
PeterCowling <124430404+PeterCowling@users.noreply.github.com>
2025-08-15 19:56:39 +0200

    fix: correct cms home imports

--- apps/cms/src/app/[lang]/page.client.tsx
+++ apps/cms/src/app/[lang]/page.client.tsx
@@ -1,8 +1,8 @@
 // apps/cms/src/app/[lang]/page.tsx
 "use client";
 
-import ReviewsCarousel from "@/components/home/ReviewsCarousel";
-import { ValueProps } from "@/components/home/ValueProps";
+import ReviewsCarousel from "@ui/components/home/ReviewsCarousel";
+import { ValueProps } from "@ui/components/home/ValueProps";
 import HeroBanner from "@ui/components/home/HeroBanner.client";
 
 export default function Home() {

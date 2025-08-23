3428cf21a567019b055366a2b56c224d5e81e568
PeterCowling <124430404+PeterCowling@users.noreply.github.com>
2025-08-23 14:18:16 +0200

    chore(cms): remove compiled js artifacts

--- apps/cms/src/app/cms/segments/page.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = SegmentsPage;
-var SegmentBuilder_1 = require("./SegmentBuilder");
-function SegmentsPage() {
-    return (<div className="p-4">
-      <SegmentBuilder_1.default />
-    </div>);
-}
--- apps/cms/src/app/cms/wizard/tokenUtils.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// apps/cms/src/app/cms/wizard/tokenUtils.ts
-/* eslint-disable import/consistent-type-specifier-style */
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.loadThemeTokens = exports.baseTokens = void 0;
-var themeTokens_1 = require("@platform-core/themeTokens");
-Object.defineProperty(exports, "baseTokens", { enumerable: true, get: function () { return themeTokens_1.baseTokens; } });
-Object.defineProperty(exports, "loadThemeTokens", { enumerable: true, get: function () { return themeTokens_1.loadThemeTokensBrowser; } });

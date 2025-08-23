83f7a791f1594f54168b31077722c108be694389
PeterCowling <124430404+PeterCowling@users.noreply.github.com>
2025-08-23 14:34:01 +0200

    chore(cms): remove generated js outputs

--- apps/cms/src/auth/options.d.ts
+++
@@ -1,3 +0,0 @@
-import type { NextAuthOptions } from "next-auth";
-export declare const authOptions: NextAuthOptions;
-//# sourceMappingURL=options.d.ts.map
--- apps/cms/src/auth/roles.d.ts
+++
@@ -1,3 +0,0 @@
-import type { Role } from "@auth";
-export type { Role };
-//# sourceMappingURL=roles.d.ts.map

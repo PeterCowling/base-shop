f92b513b58fccfdada78a144d440703212eb3c3c
PeterCowling <124430404+PeterCowling@users.noreply.github.com>
2025-08-26 19:18:20 +0200

    test: mock currency context with acme alias

--- packages/ui/src/components/organisms/__tests__/ProductCard.test.tsx
+++ packages/ui/src/components/organisms/__tests__/ProductCard.test.tsx
@@ -4,7 +4,7 @@ import type { SKU } from "@acme/types";
 import "@testing-library/jest-dom";
 import "../../../../../../test/resetNextMocks";
 
-jest.mock("@platform-core/contexts/CurrencyContext", () => ({
+jest.mock("@acme/platform-core/contexts/CurrencyContext", () => ({
   useCurrency: () => ["USD", jest.fn()],
 }));
 

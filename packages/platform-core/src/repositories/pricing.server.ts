1cea02b7ec32852641f8648b82da649cdf8ec5bb
PeterCowling <124430404+PeterCowling@users.noreply.github.com>
2025-08-26 09:24:39 +0200

    Use node fs to enable pricing tests to mock filesystem

--- packages/platform-core/src/repositories/pricing.server.ts
+++ packages/platform-core/src/repositories/pricing.server.ts
@@ -1,7 +1,7 @@
 import "server-only";
 
 import { pricingSchema, type PricingMatrix } from "@acme/types";
-import { promises as fs } from "fs";
+import { promises as fs } from "node:fs";
 import * as path from "path";
 import { resolveDataRoot } from "../dataRoot";
 

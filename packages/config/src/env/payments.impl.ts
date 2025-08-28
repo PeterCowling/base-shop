3c813ec6a07c302ad99019c7d0a918301c3b969b
PeterCowling <124430404+PeterCowling@users.noreply.github.com>
2025-08-28 07:28:14 +0200

    chore(config): default stripe env vars

--- packages/config/src/env/payments.impl.ts
+++ packages/config/src/env/payments.impl.ts
@@ -2,9 +2,12 @@ import "@acme/zod-utils/initZod";
 import { z } from "zod";
 
 export const paymentEnvSchema = z.object({
-  STRIPE_SECRET_KEY: z.string().min(1),
-  NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: z.string().min(1),
-  STRIPE_WEBHOOK_SECRET: z.string().min(1),
+  STRIPE_SECRET_KEY: z.string().min(1).default("sk_test"),
+  NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: z
+    .string()
+    .min(1)
+    .default("pk_test"),
+  STRIPE_WEBHOOK_SECRET: z.string().min(1).default("whsec_test"),
 });
 
 const parsed = paymentEnvSchema.safeParse(process.env);
@@ -17,9 +20,5 @@ if (!parsed.success) {
 
 export const paymentEnv = parsed.success
   ? parsed.data
-  : {
-      STRIPE_SECRET_KEY: "sk_test",
-      NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: "pk_test",
-      STRIPE_WEBHOOK_SECRET: "whsec_test",
-    };
+  : paymentEnvSchema.parse({});
 export type PaymentEnv = z.infer<typeof paymentEnvSchema>;

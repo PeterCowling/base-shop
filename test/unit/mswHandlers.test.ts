195b2b0b5ef3624a06370621f80badca418d6153
PeterCowling <124430404+PeterCowling@users.noreply.github.com>
2025-08-28 23:17:44 +0200

    test: verify msw handlers

---
+++ test/unit/mswHandlers.test.ts
@@ -0,0 +1,57 @@
+import { server } from "../msw/server";
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+describe("msw handlers", () => {
+  test("POST /cms/api/configurator", async () => {
+    const res = await fetch("http://localhost/cms/api/configurator", {
+      method: "POST",
+    });
+    expect(res.status).toBe(200);
+    const json = await res.json();
+    expect(json).toEqual({ success: true, message: "default handler: OK" });
+  });
+
+  test("GET /cms/api/configurator/validate-env/:shop", async () => {
+    const res = await fetch(
+      "http://localhost/cms/api/configurator/validate-env/my-shop"
+    );
+    expect(res.status).toBe(200);
+    const json = await res.json();
+    expect(json).toEqual({ success: true });
+  });
+
+  test("GET /cms/api/page-templates", async () => {
+    const res = await fetch("http://localhost/cms/api/page-templates");
+    expect(res.status).toBe(200);
+    const json = await res.json();
+    expect(json).toEqual([]);
+  });
+
+  test("GET /cms/api/wizard-progress", async () => {
+    const res = await fetch("http://localhost/cms/api/wizard-progress");
+    expect(res.status).toBe(200);
+    const json = await res.json();
+    expect(json).toEqual({ state: {}, completed: {} });
+  });
+
+  test("PUT /cms/api/wizard-progress", async () => {
+    const res = await fetch("http://localhost/cms/api/wizard-progress", {
+      method: "PUT",
+    });
+    expect(res.status).toBe(200);
+    const json = await res.json();
+    expect(json).toEqual({});
+  });
+
+  test("PATCH /cms/api/wizard-progress", async () => {
+    const res = await fetch("http://localhost/cms/api/wizard-progress", {
+      method: "PATCH",
+    });
+    expect(res.status).toBe(200);
+    const json = await res.json();
+    expect(json).toEqual({});
+  });
+});

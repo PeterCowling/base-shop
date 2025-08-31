157ddb98988a10eb9310510cb0fbbec528226fd5
PeterCowling <peter.cowling1976@gmail.com>
2025-08-31 12:15:35 +0200

    remove js from api and cms dist

---
+++ packages/template-app/src/app/not-found.tsx
@@ -0,0 +1,7 @@
+// packages/template-app/src/app/not-found.tsx
+"use client";
+import { Error404Template } from "@ui/components/templates/Error404Template";
+
+export default function NotFound() {
+  return <Error404Template />;
+}

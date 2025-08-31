977f66b0b50c55189c89c69906180702b6f3880d
PeterCowling <peter.cowling1976@gmail.com>
2025-08-31 17:51:11 +0200

    remove js from api and cms dist

--- packages/configurator/src/providers.d.ts
+++
@@ -1,7 +0,0 @@
-export interface Provider {
-    id: string;
-    name: string;
-    type: 'payment' | 'shipping' | 'analytics';
-}
-export declare const providers: Provider[];
-export declare function providersByType(type: Provider['type']): Provider[];

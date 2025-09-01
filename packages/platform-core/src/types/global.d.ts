caf8acedb56ab5941bf27c88b5e22541112558fb
PeterCowling <peter.cowling1976@gmail.com>
2025-09-01 22:01:31 +0200

    remove js from api and cms dist

---
+++ packages/platform-core/src/types/global.d.ts
@@ -0,0 +1,5 @@
+// packages/platform-core/src/types/global.d.ts
+declare module '@acme/platform-core/services/emailService';
+declare module 'dompurify';
+declare module 'resend';
+declare module 'chrome-launcher';

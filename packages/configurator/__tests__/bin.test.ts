f6aceea8e38d665ea300d0eeac6feae62be1b0d7
PeterCowling <124430404+PeterCowling@users.noreply.github.com>
2025-09-02 14:45:38 +0200

    test(configurator): handle successful dist import

--- packages/configurator/__tests__/bin.test.ts
+++ packages/configurator/__tests__/bin.test.ts
@@ -1,17 +1,23 @@
 const err = new Error("boom");
 
-jest.mock("../dist/index.js", () => {
-  throw err;
-}, { virtual: true });
-
 describe("configurator bin", () => {
   afterEach(() => {
+    jest.resetModules();
+    jest.dontMock("../dist/index.js");
     process.exitCode = undefined;
   });
 
   it("logs and sets exit code when dist import fails", async () => {
     const errorSpy = jest.spyOn(console, "error").mockImplementation(() => {});
     let p: Promise<unknown>;
+
+    jest.doMock(
+      "../dist/index.js",
+      () => {
+        throw err;
+      },
+      { virtual: true },
+    );
 
     jest.isolateModules(() => {
       p = import("../bin/configurator.js");
@@ -24,6 +30,24 @@ describe("configurator bin", () => {
       expect.anything(),
     );
     expect(process.exitCode).toBe(1);
+
+    errorSpy.mockRestore();
+  });
+
+  it("does not log or set exit code when dist import succeeds", async () => {
+    const errorSpy = jest.spyOn(console, "error").mockImplementation(() => {});
+    let p: Promise<unknown>;
+
+    jest.doMock("../dist/index.js", () => ({}), { virtual: true });
+
+    jest.isolateModules(() => {
+      p = import("../bin/configurator.js");
+    });
+
+    await p;
+
+    expect(errorSpy).not.toHaveBeenCalled();
+    expect(process.exitCode).toBeUndefined();
 
     errorSpy.mockRestore();
   });

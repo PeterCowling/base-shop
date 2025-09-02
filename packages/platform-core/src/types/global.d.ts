eea48580f6cf37af91af60a0ff6852aeae92a7b1
PeterCowling <124430404+PeterCowling@users.noreply.github.com>
2025-09-02 13:53:05 +0200

    refactor: remove ambient module declarations

--- packages/platform-core/src/types/global.d.ts
+++ packages/platform-core/src/types/global.d.ts
@@ -1,5 +1,2 @@
 // packages/platform-core/src/types/global.d.ts
 declare module '@acme/platform-core/services/emailService';
-declare module 'dompurify';
-declare module 'resend';
-declare module 'chrome-launcher';

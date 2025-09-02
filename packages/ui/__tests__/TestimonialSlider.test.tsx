8fbd548ef6fb65d0800c760c1246445ca630d4be
PeterCowling <124430404+PeterCowling@users.noreply.github.com>
2025-09-02 19:10:50 +0200

    test: fix TestimonialSlider import path

--- packages/ui/__tests__/TestimonialSlider.test.tsx
+++ packages/ui/__tests__/TestimonialSlider.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen, act } from "@testing-library/react";
-import TestimonialSlider from "../components/cms/blocks/TestimonialSlider";
+import TestimonialSlider from "../src/components/cms/blocks/TestimonialSlider";
 
 describe("TestimonialSlider", () => {
   beforeEach(() => {

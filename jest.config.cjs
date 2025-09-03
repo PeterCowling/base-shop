e9d642884e040ae832145f1a703f693880198c75
PeterCowling <124430404+PeterCowling@users.noreply.github.com>
2025-09-03 21:32:45 +0200

    Map @acme/platform-core to source in Jest

--- jest.config.cjs
+++ jest.config.cjs
@@ -176,6 +176,8 @@ const config = {
     '^@acme/config/env/(.*)$': ' /packages/config/src/env/$1.ts',
     '^@acme/config$': ' /packages/config/src/env/index.ts',
     '^@acme/config/(.*)$': ' /packages/config/src/$1',
+    '^@acme/platform-core$': ' /packages/platform-core/src/index.ts',
+    '^@acme/platform-core/(.*)$': ' /packages/platform-core/src/$1',
     '^@acme/platform-machine/src/(.*)$': ' /packages/platform-machine/src/$1',
     '^@acme/plugin-sanity$': ' /test/__mocks__/pluginSanityStub.ts',
     '^@acme/plugin-sanity/(.*)$': ' /test/__mocks__/pluginSanityStub.ts',

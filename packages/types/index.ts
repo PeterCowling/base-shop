ea9e0260e70275994e4c6e2ceee66ef260f97a0b
PeterCowling <124430404+PeterCowling@users.noreply.github.com>
2025-09-05 21:21:00 +0200

    chore(types): remove redundant root index file

--- packages/types/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./src/index";
-export { ExampleProps } from "./src/ExampleProps";
-export { historyStateSchema } from "./src/page/page";
-export type { CartLine, CartState } from "./src/Cart";

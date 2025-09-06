50e77eb3fd13120f7402cac25c7dd3d6618639a8
PeterCowling <124430404+PeterCowling@users.noreply.github.com>
2025-09-06 11:18:13 +0200

    feat(template-app): refine auth types

--- packages/template-app/types-compat/auth.d.ts
+++ packages/template-app/types-compat/auth.d.ts
@@ -1,4 +1,10 @@
+import type { CustomerSession } from "@acme/auth";
+
 declare module "@auth" {
-  export async function getCustomerSession(...args: any[]): Promise<any>;
-  export async function requirePermission(...args: any[]): Promise<void>;
+  export async function getCustomerSession(
+    ...args: any[]
+  ): Promise<CustomerSession | null>;
+  export async function requirePermission(
+    ...args: any[]
+  ): Promise<CustomerSession>;
 }

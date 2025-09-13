4abe2834d6cb057d6fcf536b9dbb3c79e94fbc2f
PeterCowling <124430404+PeterCowling@users.noreply.github.com>
2025-09-13 15:09:39 +0200

    test(ui): cover FooterBlock link groups and social links

--- packages/ui/src/components/cms/blocks/FooterBlock.tsx
+++ packages/ui/src/components/cms/blocks/FooterBlock.tsx
@@ -1,15 +1,37 @@
 import type { Locale } from "@acme/i18n/locales";
 import { Footer, type FooterLink } from "../../organisms/Footer";
 import type { LogoVariants } from "../../organisms/types";
+import SocialLinks, { type SocialLinksProps } from "./SocialLinks";
+
+interface LinkGroup {
+  title?: string;
+  links?: FooterLink[];
+}
 
 interface Props {
   links?: FooterLink[];
+  linkGroups?: LinkGroup[];
+  socialLinks?: SocialLinksProps;
   logoVariants?: LogoVariants;
   shopName: string;
   locale: Locale;
 }
 
 /** CMS wrapper for the Footer organism */
-export default function FooterBlock({ links = [], logoVariants, shopName }: Props) {
-  return <Footer links={links} logoVariants={logoVariants} shopName={shopName} />;
+export default function FooterBlock({
+  links = [],
+  linkGroups = [],
+  socialLinks,
+  logoVariants,
+  shopName,
+}: Props) {
+  const groupedLinks = linkGroups.flatMap((g) => g.links ?? []);
+  const footerLinks = groupedLinks.length ? groupedLinks : links;
+
+  return (
+    <div>
+      <Footer links={footerLinks} logoVariants={logoVariants} shopName={shopName} />
+      {socialLinks && <SocialLinks {...socialLinks} className="mt-4" />}
+    </div>
+  );
 }

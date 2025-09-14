766273119535b58e594269edfed83947dc037180
PeterCowling <124430404+PeterCowling@users.noreply.github.com>
2025-09-14 13:17:08 +0200

    chore: restore segshop campaigns data

--- apps/cms/jest.config.cjs
+++ apps/cms/jest.config.cjs
@@ -33,6 +33,7 @@ module.exports = {
     "^packages/config/src/env/core\\.ts$": "<rootDir>/packages/config/src/env/__tests__/core.stub.ts",
     "^@/components/(.*)$": "<rootDir>/test/__mocks__/componentStub.js",
     "^@/i18n/Translations$": "<rootDir>/test/emptyModule.js",
+    "^@/contexts/(.*)$": "<rootDir>/packages/platform-core/src/contexts/$1",
     "^@/(.*)$": "<rootDir>/apps/cms/src/$1",
     "^packages/config/src/env/core\\.js$": "<rootDir>/packages/config/src/env/core.ts",
     "^packages/config/src/env/index\\.js$": "<rootDir>/packages/config/src/env/index.ts",

236dc18faefd78321ee1a33f2c1a18df50a7d2e1
PeterCowling <peter.cowling1976@gmail.com>
2025-09-16 16:35:09 +0200

    remove js from api and cms dist

---
+++ packages/types/src/style/StyleOverrides.d.ts
@@ -0,0 +1,14 @@
+export interface StyleOverrides {
+    color?: {
+        fg?: string;
+        bg?: string;
+        border?: string;
+    };
+    typography?: {
+        fontFamily?: string;
+        fontSize?: string;
+        fontWeight?: string;
+        lineHeight?: string;
+    };
+}
+//# sourceMappingURL=StyleOverrides.d.ts.map

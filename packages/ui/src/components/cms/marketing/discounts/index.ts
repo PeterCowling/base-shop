a98d29fc1f50a9c056c6c62f5730884ffea83aa9
PeterCowling <124430404+PeterCowling@users.noreply.github.com>
2025-09-18 10:21:57 +0200

    Refactor discount form state management

--- packages/ui/src/components/cms/marketing/discounts/index.ts
+++ packages/ui/src/components/cms/marketing/discounts/index.ts
@@ -1,4 +1,6 @@
 export * from "./DiscountForm";
+export * from "./DiscountFieldGroups";
 export * from "./DiscountPreviewPanel";
 export * from "./DiscountSummaryCard";
 export * from "./types";
+export * from "./hooks/useDiscountFormState";

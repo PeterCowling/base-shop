6d9860dbef0eef2df77ca7e588ae28ae9fe84faa
PeterCowling <peter.cowling1976@gmail.com>
2026-01-21 11:48:41 +0100

    chore: ignore build outputs

--- apps/prime/out/_next/static/chunks/app/signage/find-my-stay-qr/page-e3d5e4b3eea416e9.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[952],{7318:()=>{}},_=>{var e=e=>_(_.s=e);_.O(0,[493,597,358],()=>e(7318)),_N_E=_.O()}]);
--- apps/skylar/out/_next/static/NF2nNzTA-FtuP1jFV8m66/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F[lang]","\u002F[lang]\u002Fpeople","\u002F[lang]\u002Fproducts","\u002F[lang]\u002Freal-estate"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()

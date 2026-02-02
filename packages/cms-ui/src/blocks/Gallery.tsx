d36ec08bec22de15a740abe7824fc6d0a1430909
PeterCowling <peter.cowling1976@gmail.com>
2026-02-02 11:50:58 +0100

    Avoid export star in cms-ui blocks

--- packages/cms-ui/src/blocks/Gallery.tsx
+++ packages/cms-ui/src/blocks/Gallery.tsx
@@ -1,4 +1,4 @@
 "use client";
 
+export type { GalleryImage } from "@acme/ui/components/cms/blocks/Gallery";
 export { default } from "@acme/ui/components/cms/blocks/Gallery";
-export * from "@acme/ui/components/cms/blocks/Gallery";

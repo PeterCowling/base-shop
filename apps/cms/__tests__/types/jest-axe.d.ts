abe0cc6fc9b4b8cd0b702dd9cd99ae3f8bbf047d
PeterCowling <peter.cowling1976@gmail.com>
2026-02-21 11:20:48 +0100

    fix cms test typings for jest-axe and jest-mock

--- apps/cms/__tests__/types/jest-axe.d.ts
+++ apps/cms/__tests__/types/jest-axe.d.ts
@@ -2,3 +2,15 @@ declare module "jest-axe" {
   export function axe(html: Element | Document | string): Promise<any>;
   export function toHaveNoViolations(results: any): { pass: boolean; message: () => string };
 }
+
+declare namespace jest {
+  interface Matchers<R> {
+    toHaveNoViolations(): R;
+  }
+}
+
+declare module "expect" {
+  interface Matchers<R> {
+    toHaveNoViolations(): R;
+  }
+}

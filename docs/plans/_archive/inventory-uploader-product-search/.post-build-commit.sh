8f5f261d65f875aa81f7fb9b1d9f55a9b8537993
PeterCowling <peter.cowling1976@gmail.com>
2026-03-13 19:08:21 +0100

    chore(inventory-uploader-product-search): post-build artifacts + archive

---
+++ docs/plans/_archive/inventory-uploader-product-search/.post-build-commit.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+git add docs/plans/_archive/inventory-uploader-product-search/ docs/business-os/startup-loop/ideas/trial/queue-state.json docs/business-os/process-improvements.user.html
+git commit -m "chore(inventory-uploader-product-search): post-build artifacts + archive"
